63c6cd83854aace4111d5e73ec59aa320918bd59
00Corby <89601291+00Corby@users.noreply.github.com>
2021-12-02 22:02:33 -0500

    Finishing Touches

--- Levels/Portal.gd
+++ Levels/Portal.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
-
 func _ready():
 	pass 
 
 func _on_Portal_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene("res://Levels/Game_Over.tscn")
+		get_tree().change_scene("res://Levels/Win.tscn")
