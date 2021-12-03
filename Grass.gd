6b80eb69c95f1bada881e1abb38d9af3ccfecde8
DisasterPC <89499424+DisasterPC@users.noreply.github.com>
2021-12-02 19:18:07 -0500

    i hate github

--- Grass.gd
+++ Grass.gd
@@ -5,7 +5,7 @@ export var score = 200
 func _ready():
 	pass
 
-func _on_Herb_body_entered(body):
+func _on_Grass_body_entered(body):
 	if body.name == "Player":
 		Global.increase_score(score)
 		queue_free()
